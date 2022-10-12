7f7ab271b32ca279a046ada298869f1ae1002101
Kaia <danielloewito@gmail.com>
2022-10-12 19:00:11 +1100

    composer

--- index.php
+++ index.php
@@ -2,3 +2,5 @@
 $sPath = realpath( __DIR__ . '/HTML-CSS-JavaScript-Project/HTML/index.html');
 include_once $sPath;
 ?>
+
+mongodb+srv://Heroku:<w7rosgQx3nHmF6hX>@resumewebsitedetails.hldsi5v.mongodb.net/?retryWrites=true&w=majority
