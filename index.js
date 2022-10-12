db34f6e2073168b6e57daef7ac6a7c93f3b48c28
Kaia <danielloewito@gmail.com>
2022-10-12 19:30:34 +1100

    dsf

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-document.getElementById("thing1").onclick = function () { "hello"; };
+document.getElementById("thing1").onclick = function () { alert("hello"); };
 
 
-//require('dotenv').config()
+require('dotenv').config()
 
-//const test = process.env.test
-//document.getElementById("thing").onclick = function () { alert(test); };
+const test = process.env.test
+document.getElementById("thing").onclick = function () { alert(test); };
 
 //mongodb+srv://Heroku:w7rosgQx3nHmF6hX@resumewebsitedetails.hldsi5v.mongodb.net/ResumeWebsiteDetails?retryWrites=true&w=majority
