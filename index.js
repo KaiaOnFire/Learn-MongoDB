fcfb7a4a8367dd217c4e89e363c1c3ce5d714793
Kaia <danielloewito@gmail.com>
2022-10-12 19:18:39 +1100

    dsGH

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const uri = process.env.MONGODB_URI;
-
 document.getElementById("thing").onclick = function () { alert("hello"); };
+
+//mongodb+srv://Heroku:w7rosgQx3nHmF6hX@resumewebsitedetails.hldsi5v.mongodb.net/ResumeWebsiteDetails?retryWrites=true&w=majority
