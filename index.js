ec3d9fa20993530a29482e78f2a4494a4159050b
Kaia <danielloewito@gmail.com>
2022-10-12 19:21:37 +1100

    dgs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-document.getElementById("thing").onclick = function () { alert("hello"); };
+const test = process.env.test
+document.getElementById("thing").onclick = function () { alert(test); };
 
 //mongodb+srv://Heroku:w7rosgQx3nHmF6hX@resumewebsitedetails.hldsi5v.mongodb.net/ResumeWebsiteDetails?retryWrites=true&w=majority
