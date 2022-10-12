d7fa7325176ae275a2eb9503baedbcaf39f94ebd
Kaia <danielloewito@gmail.com>
2022-10-13 02:30:45 +1100

    DID IT fkldgkfjldssdklsdkl

--- public/main.js
+++ public/main.js
@@ -1,18 +1,34 @@
 const updateOne = document.querySelector("#updateBtn1")
 const updateTwo = document.querySelector("#updateBtn2")
 
+function usernameCheck() {
+    var uname = document.getElementById('usernameCreate').value;
+    if(uname == ""){
+        return false;    // in failure case
+    }        
+    return true;    // in success case
+}
+
 updateOne.addEventListener("click", _ => {
+
+    var user = document.getElementById("usernamePut")
+    var msg = document.getElementById("msgPut")
+
     fetch("/updateMessage", {
         method: "put",
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
-            username: document.getElementById("usernamePut").value,
-            message: document.getElementById("messagePut").value
+            username: user.value,
+            message: msg.value
         })
     })
+
+    user.value = "";
+    msg.value = "";
 })
 
 updateTwo.addEventListener("click", _ => {
+    console.log("test")
     fetch("/updateMessage", {
         method: "put",
         headers: {'Content-Type': 'application/json'},
